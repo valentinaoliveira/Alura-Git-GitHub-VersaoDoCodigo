72d0ffb2610030cda198a38f1d046ef83c17a125
valentinaoliveira <voli.leite05@outlook.com>
2025-03-25 15:00:55 -0300

    Aidiconando linha

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ let numeroLimite = 40;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
+
+exibirMensagemInicial();
+
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -14,7 +17,6 @@ function exibirMensagemInicial() {
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 
-exibirMensagemInicial();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
